9706cccb4ef3a4e1a39d62a222c3fa26541d952b
galkahana <gal.kahana@hotmail.com>
2022-06-05 17:06:17 +0300

    and now i can put back the import sorting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,37 +1,6 @@
 import 'dotenv/config'
 import 'module-alias/register'
-
-import {init as initLog} from '@lib/logger'
-import { configure } from '@setup'
-import config from 'config'
-import express from 'express'
-import winston from 'winston'
-import { setup as terminatorSetup } from './terminator'
-
-// setup ultimate process killer
-terminatorSetup()
-// logging init
-initLog()
+import './app'
 
 
-const app = express()
 
-configure(app)
-    .then(() => {
-        // Yalla, let's get this party started!
-        const port: string = config.get('defaultPort')
-        app.set('port',  port)
-        app.listen(app.get('port'), () => {
-            winston.info(
-                `Express server listening on port ${port}`
-            )            
-        }).on('error', (ex: Error)=> {
-            winston.error(
-                `Express server failed to listen on port ${port}. ${ex}`
-            )
-        })
-    })
-    .catch((ex: Error)  => {
-        winston.info(`Server Setup error ${ex}`)
-    })
- 
