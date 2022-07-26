91154fdb6407b40f768c91eb18d79539d1d8dee7
gal kahana <gal.kahana@hotmail.com>
2022-07-26 10:55:34 +0300

    add entry/exit attributes to logged message, and duration+resource to exit log

--- backend/src/middlewares/log-request.ts
+++ backend/src/middlewares/log-request.ts
@@ -1,18 +1,21 @@
 import { Handler, Request, Response } from 'express'
 import winston from 'winston'
 
-export const logRequest: Handler = (req, _res, next) => {
+export const logRequest: Handler = (req, res, next) => {
     if (shouldSkip(req)) {
         return next()
     }
-    winston.info(`${req.method} ${req.originalUrl}`)
+    res.locals.startTime = new Date()
+    winston.info(`${req.method} ${req.originalUrl}`, { method: req.method, path: req.originalUrl })
     return next()
 }
 
 export const logResponse = (req: Request, res: Response) => {
     if (shouldSkip(req))
         return
-    winston.info(`${req.method} ${req.originalUrl} ${res.statusCode}`)
+    const duration= (new Date().getTime() - res.locals.startTime.getTime())/1000.0
+    const resource = `${req.method} ${(req.route && req.route.path) ? req.route.path: req.originalUrl}`
+    winston.info(`${req.method} ${req.originalUrl} ${res.statusCode}`, { method: req.method, path: req.originalUrl, status: res.statusCode, duration, resource })
 }
 
 const shouldSkip = (req: Request) => (req.method === 'OPTIONS')
