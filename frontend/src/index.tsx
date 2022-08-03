728b4a2c1a4da2ca66004e352dde1678c0aefdfb
galkahana <gal.kahana@hotmail.com>
2022-08-03 11:35:16 +0300

    frontend infrastracture

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from 'components/app/App'
 import reportWebVitals from 'lib/reportWebVitals'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 const root = ReactDOM.createRoot(
