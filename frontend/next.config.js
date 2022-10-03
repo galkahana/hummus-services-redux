bc38ff00db541479cec35ec5fbbd1f2c3bc9c6c0
galkahana <gal.kahana@hotmail.com>
2022-10-03 16:15:19 +0300

    drop image optimization due to using next export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,7 @@ module.exports = {
     },
     trailingSlash: true,
     images: {
+        unoptimized: true, // This one is due to using "export". when using with native next mode of running a server with `next start` you can drop this
         remotePatterns: [
           {
             protocol: 'https',
