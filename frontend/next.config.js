3890402a6f042a4a2fe71c46d35adddaa5ec3f9c
galkahana <gal.kahana@hotmail.com>
2022-10-15 13:55:39 +0300

    base mdx integration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
-module.exports = {
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
+module.exports = withMDX({
     compiler: {
         styledComponents: true
     },
@@ -12,4 +22,6 @@ module.exports = {
           },
         ],
       },
-}
+    // Append the default value with md extensions
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
